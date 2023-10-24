305b1f321604d3b2a6ee0f748918fd531fb98804
Martin Munene <munene02@gmail.com>
2023-10-24 15:53:12 +0300

    refactored controller and service

--- src/main/java/ke/co/evokemedia/spring6restmvc/controllers/BeerController.java
+++ src/main/java/ke/co/evokemedia/spring6restmvc/controllers/BeerController.java
@@ -5,15 +5,27 @@ import ke.co.evokemedia.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
-    public Beer getBeerById(UUID id){
-        log.debug("Get Beer by Id in controller");
+
+    @GetMapping
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+    @GetMapping("/{beerId}")
+    public Beer getBeerById(@PathVariable("beerId") UUID id){
+        log.debug("Get Beer by Id in controller-123");
         return beerService.getBeerById(id);
     }
 }
--- src/main/java/ke/co/evokemedia/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/ke/co/evokemedia/spring6restmvc/services/BeerServiceImpl.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
-    @Override
-    public Beer getBeerById(UUID id) {
-        log.debug(("GET Beer Id in service was called"));
-        return Beer.builder()
-                .id(id)
+    private Map<UUID, Beer> beerMap;
+    public  BeerServiceImpl() {
+        this.beerMap = new HashMap<>();
+
+        Beer beer1 = Beer.builder()
+                .id(UUID.randomUUID())
                 .version(1)
                 .beerName("Nyatipa")
                 .beerStyle(BeerStyle.ALE)
@@ -26,5 +27,43 @@ public class BeerServiceImpl implements BeerService {
                 .createdDate(LocalDateTime.now())
                 .updateDate(LocalDateTime.now())
                 .build();
+
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Tusker")
+                .beerStyle(BeerStyle.LAGER)
+                .upc("123456")
+                .price(new BigDecimal("16.99"))
+                .quantityOnHand(250)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Castle")
+                .beerStyle(BeerStyle.STOUT)
+                .upc("123457")
+                .price(new BigDecimal("18.99"))
+                .quantityOnHand(105)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put(beer1.getId(), beer1);
+        beerMap.put(beer2.getId(), beer2);
+        beerMap.put(beer3.getId(), beer3);
+    }
+    @Override
+    public List<Beer> listBeers(){
+        return new ArrayList<>(beerMap.values());
+    }
+
+    @Override
+    public Beer getBeerById(UUID id) {
+        log.debug(("GET Beer Id in service was called"));
+        return beerMap.get(id);
     }
 }
